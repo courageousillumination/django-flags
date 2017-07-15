9c9d1e906edbeef2fb7f463980cd6c1b3455c1e2
Tristan Rasmussen <tristanr93@gmail.com>
2017-07-15 16:17:23 -0700

    Another go at coverage.

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -2,4 +2,4 @@
 mkdir -p build
 env/bin/mypy --junit-xml=build/python-mypy.xml --ignore-missing-imports flags ||:
 env/bin/pylint --reports=n --rcfile=config/pylint.rc --output-format=parseable flags/*/ > build/python-lint.txt ||:
-env/bin/python runtests.py --with-xunit --xunit-file=build/python-tests.xml --coverage --cover-xml --cover-xml-file=build/python-coverage.xml ||:
+env/bin/python runtests.py --with-xunit --xunit-file=build/python-tests.xml --cover-xml --cover-xml-file=build/python-coverage.xml ||:
