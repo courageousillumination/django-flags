e192a28f6d0158ee741d04839d2ddcfbd4941c3d
Tristan Rasmussen <tristanr93@gmail.com>
2017-07-15 17:40:03 -0700

    Tweak the CI test.sh a bit.

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p build
 env/bin/mypy --junit-xml=build/python-mypy.xml --ignore-missing-imports flags ||:
-env/bin/pylint --reports=n --rcfile=config/pylint.rc --output-format=parseable flags/*/ > build/python-lint.txt ||:
+env/bin/pylint --reports=n --rcfile=config/pylint.rc --output-format=parseable flags/*.py flags/tests > build/python-lint.txt ||:
 env/bin/python runtests.py flags --with-xunit --xunit-file=build/python-tests.xml --cover-xml --cover-xml-file=build/python-coverage.xml ||:
